70ce3877dfea2a7282b11b9bb8a57cf1543bfa18
Anthony Boccadoro <aboccadoro@umass.edu>
2019-04-03 12:54:03 -0400

    Stage 3 implemented: parse title in HTML

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Pattern;
 
 public class WebCrawler extends JFrame {
 
@@ -17,10 +18,14 @@ public class WebCrawler extends JFrame {
         setTitle("Web Crawler");
         setVisible(true);
 
-        var site_url = new JTextField();
+        var site_url = new JTextField("http://www.example.com");
         site_url.setName("UrlTextField");
         var download = new JButton("Get HTML");
         download.setName("RunButton");
+        var url_label = new JLabel("URL:  ");
+        var title_label = new JLabel("Title:  ");
+        var title_label_text = new JLabel();
+        title_label_text.setName("TitleLabel");
 
         var user_input_pane = new JPanel();
         GroupLayout user_input_layout = new GroupLayout(user_input_pane);
@@ -29,19 +34,28 @@ public class WebCrawler extends JFrame {
         user_input_layout.setAutoCreateContainerGaps(true);
         user_input_layout.setHorizontalGroup(
                 user_input_layout.createSequentialGroup()
-                        .addComponent(site_url)
+                        .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(url_label)
+                                .addComponent(title_label)
+                        )
+                        .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(site_url)
+                                .addComponent(title_label_text)
+                        )
                         .addComponent(download)
-                        .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.LEADING))
         );
         user_input_layout.setVerticalGroup(
                 user_input_layout.createSequentialGroup()
-                        .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(url_label)
                                 .addComponent(site_url)
                                 .addComponent(download)
                         )
+                        .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(title_label)
+                                .addComponent(title_label_text)
+                        )
         );
-        user_input_pane.add(site_url);
-        user_input_pane.add(download);
         add(user_input_pane, BorderLayout.PAGE_START);
 
         var html = new JTextArea();
@@ -54,17 +68,25 @@ public class WebCrawler extends JFrame {
             try {
                 final String url = site_url.getText();
 
-                final InputStream inputStream = new URL(url).openStream();
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-                final StringBuilder stringBuilder = new StringBuilder();
+                final var inputStream = new URL(url).openStream();
+                final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                final var stringBuilder = new StringBuilder();
 
                 String nextLine;
+                AtomicBoolean matched = new AtomicBoolean(false);
+                var p = Pattern.compile(".*<title>(.*)</title>.*");
                 while ((nextLine = reader.readLine()) != null) {
                     stringBuilder.append(nextLine);
                     stringBuilder.append(System.getProperty("line.separator"));
+                    var m = p.matcher(nextLine);
+                    if (!matched.get() && m.matches()) {
+                        matched.set(true);
+                        title_label_text.setText(m.group(1));
+                    }
+                    else if (m.matches()) System.out.println("multiple");
                 }
 
-                final String siteText = stringBuilder.toString();
+                final var siteText = stringBuilder.toString();
                 html.setText(siteText);
             }
             catch (IOException error) {
@@ -76,5 +98,6 @@ public class WebCrawler extends JFrame {
         setSize(800, 600);
 
         html.disable();
+        download.doClick();
     }
 }
