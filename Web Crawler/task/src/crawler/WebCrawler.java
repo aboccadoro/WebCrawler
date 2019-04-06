3543f388a1bfb45abdfb325c5c3b5241df86079e
Anthony Boccadoro <aboccadoro@umass.edu>
2019-04-05 22:12:07 -0400

    multi-line title functionality

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
 
@@ -81,34 +82,70 @@ public class WebCrawler extends JFrame {
     private void initParser(JButton parse_button, JTextField site_url, JLabel title_label_text, DefaultTableModel title_table_model) {
         parse_button.addActionListener(e -> {
             try {
-                final String url = site_url.getText();
+                final var url = site_url.getText();
+                var protocol_pattern = Pattern.compile("(?Ui)(.+)//.*");
+                var protocol_matcher = protocol_pattern.matcher(url);
+                if (!protocol_matcher.matches()) throw new IOException();
+                var protocol = protocol_matcher.group(1);
 
                 final var inputStream = new URL(url).openStream();
                 final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
-                String nextLine;
-                String link;
-                var title_matched = new AtomicBoolean(false);
-                var title_pattern = Pattern.compile("(?Ui).*<title>(.*)</title>.*");
+                StringBuilder nextLine;
+                var title_pattern = Pattern.compile("(?Ui).*<title.*>(.*)</title>.*");
+                var title_tag_pattern = Pattern.compile("(?Ui).*<title.*>.*");
                 // TODO pattern match links
-                var href_pattern = Pattern.compile("(?Ui)<[^>]*\\s*href\\s*=\\s*(\"(?:[^\"]*\")|'[^']*'|(?:[^'\">\\s]+))[^>]*>.+?</[^>]+>");
-                while ((nextLine = reader.readLine()) != null) {
-                    var title_matcher = title_pattern.matcher(nextLine);
-                    if (!title_matched.get() && title_matcher.matches()) {
-                        title_matched.set(true);
-                        title_label_text.setText(title_matcher.group(1));
+                var href_pattern = Pattern.compile("(?Ui)<[^>]+\\s+(?:href|src)=\\s*([\"'])([^\"']+)\\1[^>]*>(?:.+?</[^>]+>)?");
+                while ((nextLine = Optional.ofNullable(reader.readLine()).map(StringBuilder::new).orElse(null)) != null) {
+                    var title_tag_matcher = title_tag_pattern.matcher(nextLine);
+                    if (title_tag_matcher.matches()) {
+                        while (!nextLine.toString().contains("</title>")) {
+                            var next = reader.readLine();
+                            if (next != null) nextLine.append(next);
+                            else throw new IOException();
+                        }
                     }
+                    var title_matcher = title_pattern.matcher(nextLine);
+                    if (title_matcher.matches()) title_label_text.setText(title_matcher.group(1));
                     var href_matcher = href_pattern.matcher(nextLine);
                     if (href_matcher.matches()) {
-                        // TODO add protocol
-                        link = href_matcher.group(1);
-                        String[] data = {link.substring(1, link.length() - 1), getLinkTitle(link)};
-                        title_table_model.addRow(data);
+                        // TODO Fix href/src assembly
+                        var link = href_matcher.group(2);
+                        var relative_pattern = Pattern.compile("(?Ui)([^/]+)");
+                        var relative_matcher = relative_pattern.matcher(link);
+                        var no_protocol_pattern = Pattern.compile("(?Ui)^//.+");
+                        var no_protocol_matcher = no_protocol_pattern.matcher(link);
+                        var nosource_slash_pattern = Pattern.compile("(?Ui)^/.+");
+                        var nosource_slash_matcher = nosource_slash_pattern.matcher(link);
+                        var nosource_noslash_pattern = Pattern.compile("(?Ui)^.+/.*");
+                        var nosource_noslash_matcher = nosource_noslash_pattern.matcher(link);
+                        if (relative_matcher.matches()) {
+                            var stripped_source_link_pattern = Pattern.compile("(?Ui)(.+/)[^/]*");
+                            var stripped_source_link_matcher = stripped_source_link_pattern.matcher(url);
+                            if (stripped_source_link_matcher.matches()) link = stripped_source_link_matcher.group(1) + link;
+                        }
+                        else if (no_protocol_matcher.matches()) link = protocol + link;
+                        else if (nosource_slash_matcher.matches()) {
+                            var stripped_source_link_pattern = Pattern.compile("(?Ui)(.+)/[^/]*");
+                            var stripped_source_link_matcher = stripped_source_link_pattern.matcher(url);
+                            if (stripped_source_link_matcher.matches()) link = stripped_source_link_matcher.group(1) + link;
+                        }
+                        else if (nosource_noslash_matcher.matches()) {
+                            var stripped_source_link_pattern = Pattern.compile("(?Ui)(.+/)[^/]*");
+                            var stripped_source_link_matcher = stripped_source_link_pattern.matcher(url);
+                            if (stripped_source_link_matcher.matches()) link = stripped_source_link_matcher.group(1) + link;
+                        }
+                        final var content = new URL(link).openConnection();
+                        if (content.getContentType().contains("text/html")) {
+                            String[] data = {link, getLinkTitle(link)};
+                            title_table_model.addRow(data);
+                        }
                     }
                 }
             }
             catch (IOException error) {
                 // TODO add error popup window
+                error.printStackTrace();
             }
         });
     }
@@ -118,11 +155,17 @@ public class WebCrawler extends JFrame {
             final var inputStream = new URL(url).openStream();
             final var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
-            String nextLine;
-            var title_pattern = Pattern.compile("(?Ui).*<title>(.*)</title>.*");
-            while ((nextLine = reader.readLine()) != null) {
-                System.out.println("made it");
-                var title_matcher = title_pattern.matcher(nextLine);
+            StringBuilder nextLine;
+            var title_pattern = Pattern.compile("(?Ui).*<title.*>(.*)</title>.*");
+            var title_tag_pattern = Pattern.compile("(?Ui).*<title.*>.*");
+            while ((nextLine = Optional.ofNullable(reader.readLine()).map(StringBuilder::new).orElse(null)) != null) {
+                var title_tag_matcher = title_tag_pattern.matcher(nextLine);
+                if (title_tag_matcher.matches()) {
+                    var next = reader.readLine();
+                    if (next != null) nextLine.append(next);
+                    else throw new IOException();
+                }
+                var title_matcher = title_pattern.matcher(nextLine.toString());
                 if (title_matcher.matches()) return title_matcher.group(1);
             }
         }
