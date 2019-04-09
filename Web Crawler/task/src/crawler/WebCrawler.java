9b6e8c42a31b940a04a36576324dc9b3960d15b3
Anthony Boccadoro <aboccadoro@umass.edu>
2019-04-08 22:34:33 -0400

    Stage 4: parse all links implemented

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
 
 public class WebCrawler extends JFrame {
@@ -21,31 +20,30 @@ public class WebCrawler extends JFrame {
         setVisible(true);
 
         final var url_label = new JLabel("URL:  ");
-        final var site_url = new JTextField("https://www.wikipedia.org/");
-        site_url.setName("UrlTextField");
+        final var url_text = new JTextField("");
+        url_text.setName("UrlTextField");
         final var parse_button = new JButton("Parse");
         parse_button.setName("RunButton");
         final var title_label = new JLabel("Title:  ");
         final var title_label_text = new JLabel();
         title_label_text.setName("TitleLabel");
-        String[] column_names = {"URL", "Title"};
-        DefaultTableModel title_table_model = new DefaultTableModel(column_names, 0);
+        final String[] column_names = {"URL", "Title"};
+        final var title_table_model = new DefaultTableModel(column_names, 0);
         final var title_table = new JTable(title_table_model);
         title_table.setName("TitlesTable");
 
-        initUI(url_label, site_url, parse_button, title_label, title_label_text, title_table);
-        initParser(parse_button, site_url, title_label_text, title_table_model);
+        initUI(url_label, url_text, parse_button, title_label, title_label_text, title_table);
+        initParser(parse_button, url_text, title_label_text, title_table_model);
 
         pack();
         setSize(800, 600);
 
         title_table.disable();
-        parse_button.doClick();
     }
 
-    private void initUI(JLabel url_label, JTextField site_url, JButton parse_button, JLabel title_label, JLabel title_label_text, JTable title_table) {
+    private void initUI(JLabel url_label, JTextField url_text, JButton parse_button, JLabel title_label, JLabel title_label_text, JTable title_table) {
         final var user_input_pane = new JPanel();
-        var user_input_layout = new GroupLayout(user_input_pane);
+        final var user_input_layout = new GroupLayout(user_input_pane);
         user_input_pane.setLayout(user_input_layout);
         user_input_layout.setAutoCreateGaps(true);
         user_input_layout.setAutoCreateContainerGaps(true);
@@ -56,7 +54,7 @@ public class WebCrawler extends JFrame {
                                 .addComponent(title_label)
                         )
                         .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(site_url)
+                                .addComponent(url_text)
                                 .addComponent(title_label_text)
                         )
                         .addComponent(parse_button)
@@ -65,7 +63,7 @@ public class WebCrawler extends JFrame {
                 user_input_layout.createSequentialGroup()
                         .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                 .addComponent(url_label)
-                                .addComponent(site_url)
+                                .addComponent(url_text)
                                 .addComponent(parse_button)
                         )
                         .addGroup(user_input_layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
@@ -79,10 +77,13 @@ public class WebCrawler extends JFrame {
         add(title_table_pane, BorderLayout.CENTER);
     }
 
-    private void initParser(JButton parse_button, JTextField site_url, JLabel title_label_text, DefaultTableModel title_table_model) {
+    private void initParser(JButton parse_button, JTextField url_text, JLabel title_label_text, DefaultTableModel title_table_model) {
         parse_button.addActionListener(e -> {
             try {
-                final var url = site_url.getText();
+                while (title_table_model.getRowCount() > 0) {
+                    title_table_model.removeRow(title_table_model.getRowCount() - 1);
+                }
+                final var url = url_text.getText();
                 var protocol_pattern = Pattern.compile("(?Ui)(.+)//.*");
                 var protocol_matcher = protocol_pattern.matcher(url);
                 if (!protocol_matcher.matches()) throw new IOException();
@@ -94,8 +95,7 @@ public class WebCrawler extends JFrame {
                 StringBuilder nextLine;
                 var title_pattern = Pattern.compile("(?Ui).*<title.*>(.*)</title>.*");
                 var title_tag_pattern = Pattern.compile("(?Ui).*<title.*>.*");
-                // TODO pattern match links
-                var href_pattern = Pattern.compile("(?Ui)<[^>]+\\s+(?:href|src)=\\s*([\"'])([^\"']+)\\1[^>]*>(?:.+?</[^>]+>)?");
+                var href_pattern = Pattern.compile("(?Ui).*<a\\s(?:[^>]+//s)?href=([\"'])([^\"']+)\\1[^>]*>(?:.+?</a>)?.*");
                 while ((nextLine = Optional.ofNullable(reader.readLine()).map(StringBuilder::new).orElse(null)) != null) {
                     var title_tag_matcher = title_tag_pattern.matcher(nextLine);
                     if (title_tag_matcher.matches()) {
@@ -130,15 +130,17 @@ public class WebCrawler extends JFrame {
                             var stripped_source_link_matcher = stripped_source_link_pattern.matcher(url);
                             if (stripped_source_link_matcher.matches()) link = stripped_source_link_matcher.group(1) + link;
                         }
-                        else if (nosource_noslash_matcher.matches()) {
+                        else if (nosource_noslash_matcher.matches() && !link.contains(protocol)) {
                             var stripped_source_link_pattern = Pattern.compile("(?Ui)(.+/)[^/]*");
                             var stripped_source_link_matcher = stripped_source_link_pattern.matcher(url);
                             if (stripped_source_link_matcher.matches()) link = stripped_source_link_matcher.group(1) + link;
                         }
                         final var content = new URL(link).openConnection();
-                        if (content.getContentType().contains("text/html")) {
-                            String[] data = {link, getLinkTitle(link)};
-                            title_table_model.addRow(data);
+                        if (content.getContentType() != null) {
+                            if (content.getContentType().contains("text/html")) {
+                                String[] data = {link, getLinkTitle(link)};
+                                title_table_model.addRow(data);
+                            }
                         }
                     }
                 }
